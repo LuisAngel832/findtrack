6c4b2234e13afc7681b691e4df288b61ebb59c26
Luis Angel <luisgelipe.12340@gmail.com>
2026-04-12 21:24:21 -0600

    chore: initialize project structure

---
+++ src/main/java/com/universidad/proyecto/FindTrack/FindTrackApplication.java
@@ -0,0 +1,13 @@
+package com.universidad.proyecto.FindTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universidad/proyecto/FindTrack/FindTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.proyecto.FindTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
